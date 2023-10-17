0c782f7b8bf6ee34570cab906d55ed3fdc4c9062
Tanjungrjs <tanjungmadusaputra@gmail.com>
2023-10-17 22:25:52 +0700

    Belajar React JS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import "./App.css"
+
+function App() {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
